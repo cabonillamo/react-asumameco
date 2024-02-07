de905b29949bca5a493f008becaa529cade16e48
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-07 14:58:06 -0600

    Update App component to display a centered greeting message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App(): JSX.Element {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100 text-4xl">
+      Hello, Carlitos
+    </div>
+  );
 }
 
 export default App;
