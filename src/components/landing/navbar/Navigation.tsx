55dfd4cdb4483feade8929f484e7fc6063d26b94
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-08 02:13:31 -0600

    Add Navigation component to landing page

---
+++ src/components/landing/navbar/Navigation.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import Logo from "../Logo";
+import Button from "../ui/Button";
+
+const Section = styled.section`
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 85%;
+  height: 5rem;
+  height: ${(props) => props.theme.navHeight};
+  margin: 0 auto;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+`;
+
+const MenuItem = styled.li`
+  margin: 0 1rem;
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background-color: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+function Navigation() {
+  return (
+    <Section>
+      <NavBar>
+        <Logo />
+        <Menu>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Guide</MenuItem>
+          <MenuItem>Showcase</MenuItem>
+          <MenuItem>Teamo</MenuItem>
+          <MenuItem>Faq</MenuItem>
+        </Menu>
+       <Button text="Sign In" link="/signin" />
+      </NavBar>
+    </Section>
+  );
+}
+
+export default Navigation;
