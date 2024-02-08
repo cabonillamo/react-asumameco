47097fc9cd5b63b538af4ef18ae37b81b041cc5a
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-08 03:35:49 -0600

    Refactor App component to include styled-components and new sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from "./styles/Theme";
+import Navigation from "./components/landing/navbar/Navigation";
+import Home from "./components/landing/sections/Home";
+import About from "./components/landing/sections/About";
+import Guide from "./components/landing/sections/Guide";
+import Showcase from "./components/landing/sections/Showcase";
+import Team from "./components/landing/sections/Team";
+import Faq from "./components/landing/sections/Faq";
+import Footer from "./components/landing/footer/Footer";
+
 function App(): JSX.Element {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100 text-4xl">
-      Hello, Carlitos
-    </div>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        <Navigation />
+        <Home />
+        <About />
+        <Guide />
+        <Showcase />
+        <Team />
+        <Faq />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
 
