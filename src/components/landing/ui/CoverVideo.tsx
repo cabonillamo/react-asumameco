c67640fcee8423bbf173083c30e0bb5dec156d3d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-08 04:18:36 -0600

    Replace video with image in CoverVideo component

--- src/components/landing/ui/CoverVideo.tsx
+++ src/components/landing/ui/CoverVideo.tsx
@@ -1,30 +1,20 @@
-import HomeVideo from "../../../assets/Home Video3.mp4";
+import HomeImage from "../../../assets/Home Image.png";
 import styled from "styled-components";
 
-const VideoContainer = styled.div`
+const ImageContainer = styled.div`
   width: 100%;
-  overflow: hidden;
 `;
 
-const StyledVideo = styled.video`
+const StyledImage = styled.img`
   width: 100%;
   height: auto;
-  border-radius: 5px;
-  mask-image: linear-gradient(
-      to top,
-      transparent,
-      white 10%,
-      white 90%,
-      transparent
-    ),
-    linear-gradient(to bottom, transparent, white 10%, white 90%, transparent);
 `;
 
 function CoverVideo() {
   return (
-    <VideoContainer>
-      <StyledVideo src={HomeVideo} autoPlay muted loop />
-    </VideoContainer>
+    <ImageContainer>
+      <StyledImage src={HomeImage} />
+    </ImageContainer>
   );
 }
 
