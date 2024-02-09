54aec2c03bf1f0c6a4f9efea31e6e289f1b7ae0c
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-08 19:50:01 -0600

    Add styled components and update About section

--- src/components/landing/sections/About.tsx
+++ src/components/landing/sections/About.tsx
@@ -1,7 +1,92 @@
+import styled, { ThemeProvider } from "styled-components";
+import Carousel from "../ui/Carousel";
+import Button from "../ui/Button";
+import { dark } from "../../../styles/Theme";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const SubText = styled.p`
+  font-size: ${(props) => props.theme.fontlg};
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  width: 80%;
+  margin: 1rem auto;
+  font-weight: 400;
+`;
+
+const SubTextLight = styled.p`
+  font-size: ${(props) => props.theme.fontmd};
+  color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.6)`};
+  align-self: flex-start;
+  width: 80%;
+  margin: 1rem auto;
+  font-weight: 400;
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  margin: 1rem auto;
+  align-self: flex-start;
+`;
+
 function About() {
   return (
-    <div>About</div>
-  )
+    <Section>
+      <Container>
+        <Box>
+          <Carousel />
+        </Box>
+        <Box>
+          <Title>Welcome to the best association hub</Title>
+          <SubText>
+            This software is designed to help you manage your association with
+            ease
+          </SubText>
+          <SubTextLight>You can manage your members and events</SubTextLight>
+          <ButtonContainer>
+            <ThemeProvider theme={dark}>
+              <Button text="Join Now!" link="/pre-signup" />
+            </ThemeProvider>
+          </ButtonContainer>
+        </Box>
+      </Container>
+    </Section>
+  );
 }
 
-export default About
+export default About;
