c15f21a5cf609848cfa86fc86714083f54434f85
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-15 21:40:45 -0600

    Refactor responsive styles in About and Carousel components

--- src/components/landing/sections/About.tsx
+++ src/components/landing/sections/About.tsx
@@ -16,19 +16,44 @@ const Section = styled.section`
 const Container = styled.div`
   width: 75%;
   margin: 0 auto;
+
   /* background-color: red; */
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 70em) {
+    width: 85%;
+  }
+
+  @media (max-width: 64em) {
+    width: 100%;
+    /* margin-top: 3px; */
+    flex-direction: column;
+    & > *:last-child {
+      width: 80%;
+    }
+  }
+
+  @media (max-width: 40em) {
+    & > *:last-child {
+      width: 90%;
+    }
+  }
 `;
 
 const Box = styled.div`
   width: 50%;
   height: 100%;
+  min-height: 60vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 40em) {
+    min-height: 50vh;
+  }
 `;
 
 const Title = styled.h2`
@@ -38,6 +63,19 @@ const Title = styled.h2`
   align-self: flex-start;
   width: 80%;
   margin: 0 auto;
+
+  @media (max-width: 64em) {
+    width: 100%;
+    text-align: center;
+  }
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontxl};
+  }
+
+  @media (max-width: 30em) {
+    font-size: ${(props) => props.theme.fontlg};
+  }
 `;
 
 const SubText = styled.p`
@@ -47,6 +85,20 @@ const SubText = styled.p`
   width: 80%;
   margin: 1rem auto;
   font-weight: 400;
+
+  @media (max-width: 64em) {
+    width: 100%;
+    text-align: center;
+    font-size: ${(props) => props.theme.fontmd};
+  }
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
+
+  @media (max-width: 30em) {
+    font-size: ${(props) => props.theme.fontsm};
+  }
 `;
 
 const SubTextLight = styled.p`
@@ -56,12 +108,35 @@ const SubTextLight = styled.p`
   width: 80%;
   margin: 1rem auto;
   font-weight: 400;
+
+  @media (max-width: 64em) {
+    width: 100%;
+    text-align: center;
+    font-size: ${(props) => props.theme.fontsm};
+  }
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontsm};
+  }
+
+  @media (max-width: 30em) {
+    font-size: ${(props) => props.theme.fontxs};
+  }
 `;
 
 const ButtonContainer = styled.div`
   width: 80%;
   margin: 1rem auto;
+  display: flex;
   align-self: flex-start;
+
+  @media (max-width: 64em) {
+    width: 100%;
+
+    button {
+      margin: 0 auto;
+    }
+  }
 `;
 
 function About() {
@@ -72,7 +147,9 @@ function About() {
           <Carousel />
         </Box>
         <Box>
-          <Title>Welcome to the best association hub</Title>
+          <Title>
+            Welcome to the <br /> best association hub
+          </Title>
           <SubText>
             This software is designed to help you manage your association with
             ease
--- src/components/landing/ui/Carousel.tsx
+++ src/components/landing/ui/Carousel.tsx
@@ -17,6 +17,25 @@ const Container = styled.div`
   width: 25vw;
   height: 70vh;
 
+  @media (max-width: 70em) {
+    height: 60vh;
+  }
+
+  @media (max-width: 64em) {
+    height: 50vh;
+    width: 30vw;
+  }
+
+  @media (max-width: 48em) {
+    height: 50vh;
+    width: 40vw;
+  }
+
+  @media (max-width: 30em) {
+    height: 45vh;
+    width: 60vw;
+  }
+
   .swiper {
     width: 100%;
     height: 100%;
@@ -49,6 +68,14 @@ const Container = styled.div`
     &:after {
       display: none;
     }
+
+    @media (max-width: 64em) {
+      width: 3rem;
+    }
+
+    @media (max-width: 30em) {
+      width: 2rem;
+    }
   }
   .swiper-button-prev {
     color: ${(props) => props.theme.text};
@@ -62,6 +89,14 @@ const Container = styled.div`
 
     &:after {
       display: none;
+    }
+
+    @media (max-width: 64em) {
+      width: 3rem;
+    }
+
+    @media (max-width: 30em) {
+      width: 2rem;
     }
   }
 `;
