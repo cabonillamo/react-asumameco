d996536fc7be591cf030309e8ba7f4b32925f8ad
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-17 10:31:42 -0600

    Add LandingPage component

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,31 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
+import { light } from "../styles/Theme";
+import Navigation from "../components/landing/navbar/Navigation";
+import Home from "../components/landing/sections/Home";
+import About from "../components/landing/sections/About";
+import Guide from "../components/landing/sections/Guide";
+import Team from "../components/landing/sections/Team";
+import Faq from "../components/landing/sections/Faq";
+import Footer from "../components/landing/footer/Footer";
+import ScrollToTop from "../components/landing/ui/ScrollToTop";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={light}>
+        <Navigation />
+        <Home />
+        <About />
+        <Guide />
+        <Team />
+        <Faq />
+        <Footer />
+        <ScrollToTop />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
