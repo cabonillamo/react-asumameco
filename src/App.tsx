24e0e28e3283ec001029346312a31d5eb7c95b9a
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-17 10:32:06 -0600

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import { light } from "./styles/Theme";
-import Navigation from "./components/landing/navbar/Navigation";
-import Home from "./components/landing/sections/Home";
-import About from "./components/landing/sections/About";
-import Guide from "./components/landing/sections/Guide";
-import Team from "./components/landing/sections/Team";
-import Faq from "./components/landing/sections/Faq";
-import Footer from "./components/landing/footer/Footer";
-import ScrollToTop from "./components/landing/ui/ScrollToTop";
+import LandingPage from "./pages/LandingPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
+import { Routes, Route } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
     <>
-      <GlobalStyles />
-      <ThemeProvider theme={light}>
-        <Navigation />
-        <Home />
-        <About />
-        <Guide />
-        <Team />
-        <Faq />
-        <Footer />
-        <ScrollToTop />
-      </ThemeProvider>
+      <Routes>
+        <Route element={<ProtectedRoute isAllowed={true} redirectTo="/home" />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
