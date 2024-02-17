7953f43ebe5616290d4bab1ffe1ff13ea8185e1b
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-17 10:31:52 -0600

    Add index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement || document.createElement("div"));
