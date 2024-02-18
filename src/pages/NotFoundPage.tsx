49a843c3cd38fdd032586f7046f211e3485e203c
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-18 16:41:19 -0600

    Update import path for Card component in NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Card } from "../components/ui/Card";
+import { Card } from "../components/not-found/Card";
 
 function NotFound(): JSX.Element {
   return (
