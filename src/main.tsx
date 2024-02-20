b631de21e0753bb0f4d7086a9d7656dbb7e9f3f2
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-20 02:50:09 -0600

    Add AuthProvider and ToastContainer to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./context/provider/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement || document.createElement("div"));
 
 root.render(
   <Router>
-    <App />
+    <AuthProvider>
+      <ToastContainer />
+      <App />
+    </AuthProvider>
   </Router>
 );
