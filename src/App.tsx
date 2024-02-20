3aff96c6bb80072acf72e344ca4a4557cbfcbace
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-20 02:50:03 -0600

    Add Home page and authentication routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import LoginPage from "./pages/LoginPage";
+import Home from "./pages/HomePage";
 import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { Routes, Route } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import "react-toastify/dist/ReactToastify.css";
 
 function App(): JSX.Element {
+  const { isAuth } = useAuth();
+
   return (
     <>
       <Routes>
-        <Route element={<ProtectedRoute isAllowed={true} redirectTo="/home" />}>
+        {/* logueado */}
+        <Route
+          element={<ProtectedRoute isAllowed={!isAuth} redirectTo="/home" />}
+        >
           <Route path="/login" element={<LoginPage />} />
         </Route>
 
+        {/* no logueado */}
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} redirectTo="/login" />}
+        >
+          <Route path="/home" element={<Home />} />
+        </Route>
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
