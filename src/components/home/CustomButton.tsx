349bc6aeb15ce3fdecbf3f56c3231c66fcd65dc6
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-22 22:24:35 -0600

    Add CustomButton component

---
+++ src/components/home/CustomButton.tsx
@@ -0,0 +1,29 @@
+interface CustomButtonProps {
+  title?: string;
+  containerStyles?: string;
+  iconRight?: boolean;
+  type?: string;
+  onClick?: any;
+}
+
+function CustomButton({
+  title,
+  containerStyles,
+  iconRight,
+  type,
+  onClick,
+}: CustomButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      type={type as "submit" | "button" | "reset" | undefined}
+      className={`inline-flex items-center text-base ${containerStyles}`}
+    >
+      {title}
+
+      {iconRight && <div className="ml-2">{iconRight}</div>}
+    </button>
+  );
+}
+
+export default CustomButton;
