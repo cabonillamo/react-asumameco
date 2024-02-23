e8f9ece6d58b833f93f34743c4aabd2353f1389e
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-22 21:36:04 -0600

    Refactor main.tsx to include Redux store provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/provider/AuthProvider";
-import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement || document.createElement("div"));
 
 root.render(
-  <Router>
-    <AuthProvider>
-      <ToastContainer />
-      <App />
-    </AuthProvider>
-  </Router>
+  <div>
+    <Router>
+      <Provider store={store}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </Provider>
+    </Router>
+  </div>
 );
