5255bc5edea67b658aba457023d211186ea703ef
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-22 22:24:52 -0600

    Refactor HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
-import { useAuth } from "../hooks/useAuth";
+import TopBar from "../components/home/TopBar";
+import { useSelector } from "react-redux";
 
 function Home() {
-  const { isAuth, user } = useAuth();
-  console.log("User:", user);
+  // const { isAuth, user } = useAuth();
+  const { theme } = useSelector((state: any) => state.theme);
+  console.log(theme);
   return (
-    <div>
-      <h1>Home</h1>
-      <p>isAuth: {isAuth.toString()}</p>
-      <p>User: {user}</p>
+    <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
+      <TopBar />
     </div>
   );
 }
