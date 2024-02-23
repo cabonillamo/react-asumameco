aaf38171f7e973d16feda02ce888b51e9af3c8b6
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-02-22 23:20:58 -0600

    Add ProfileCard component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,25 @@
 import TopBar from "../components/home/TopBar";
 import { useSelector } from "react-redux";
+import { useAuth } from "../hooks/useAuth";
+import ProfileCard from "../components/home/ProfileCard";
 
 function Home() {
-  // const { isAuth, user } = useAuth();
+  const { user, userEmail } = useAuth();
   const { theme } = useSelector((state: any) => state.theme);
   console.log(theme);
   return (
     <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
       <TopBar />
+      <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
+        {/* l */}
+        <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto">
+          <ProfileCard user={user} userEmail={userEmail} />
+        </div>
+        {/* c */}
+        <div></div>
+      </div>
+      {/* r */}
+      <div></div>
     </div>
   );
 }
