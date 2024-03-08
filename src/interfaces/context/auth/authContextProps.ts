c96b187ddd6a0069aec329b6eef3349058f1ece0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-08 01:16:27 -0600

    Add new methods to authContextProps interface

--- src/interfaces/context/auth/authContextProps.ts
+++ src/interfaces/context/auth/authContextProps.ts
@@ -4,6 +4,13 @@ export interface authContextProps {
   user: User | null;
   isAuth: boolean;
   errors: string[];
+  isEmailValid: boolean;
   signIn: (data: FormData) => Promise<void>;
   signOut: () => Promise<void>;
+  resetPassword: (email: string) => Promise<void>;
+  changePassword: (
+    currentPassword: string,
+    newPassword: string,
+    token: string
+  ) => Promise<void>;
 }
