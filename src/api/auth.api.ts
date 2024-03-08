d1b4f0b2d7b9e9d0c990b843601d3e6e2695be82
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-08 01:14:43 -0600

    Add resetPasswordRequest and changePasswordRequest functions

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -7,7 +7,6 @@ export const signOutRequest = async (token: string) =>
   client.post("usuario/SignOut", null, {
     headers: { Authorization: `Bearer ${token}` },
   });
-  
 
 export const meRequest = async (token: string) => {
   try {
@@ -16,6 +15,48 @@ export const meRequest = async (token: string) => {
         Authorization: `Bearer ${token}`,
       },
     });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const resetPasswordRequest = async (email: string) => {
+  try {
+    const response = await client.post("usuario/ResetPassword", `"${email}"`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const changePasswordRequest = async (
+  email: string,
+  resetToken: string,
+  newPassword: string
+) => {
+  try {
+    const response = await client.post(
+      "usuario/ChangePassword",
+      {
+        email,
+        resetToken,
+        newPassword,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     return response.data;
   } catch (error) {
