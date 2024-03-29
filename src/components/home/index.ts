4f3d7782ff8906167bcb8b7e11758c3e722377ef
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-28 21:41:21 -0600

    Add new components and exports in home/index.ts

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -3,5 +3,15 @@ import ProfileCard from "./ProfileCard";
 import CustomButton from "./CustomButton";
 import MoreInfo from "./MoreInfo";
 import EditProfile from "./EditProfile";
+import TextInput from "./TextInput";
+import PostCard from "./Posts/PostCard";
 
-export { TopBar, ProfileCard, CustomButton, MoreInfo, EditProfile };
+export {
+  TopBar,
+  ProfileCard,
+  CustomButton,
+  MoreInfo,
+  EditProfile,
+  TextInput,
+  PostCard,
+};
