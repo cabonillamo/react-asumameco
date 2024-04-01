1d4d23d86d6988d96130f221b87ce5ff3c7a9398
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-31 20:24:07 -0600

    Add createEvent function to eventsContextProps interface

--- src/interfaces/context/events/eventsContextProps.tsx
+++ src/interfaces/context/events/eventsContextProps.tsx
@@ -3,4 +3,11 @@ import { Event } from "../../../interfaces/context/events/event";
 export interface eventsContextProps {
   events: Event[];
   loadEvents: () => void;
+  createEvent: (
+    nombre: string,
+    direccion: string,
+    fecha: string,
+    descripcion: string,
+    imagen: File
+  ) => void;
 }
